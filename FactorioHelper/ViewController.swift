61e4fb1c9ba1d263454579b324fe018b6d1306a7
Сергей Василенко <kudrykun@gmail.com>
2020-09-03 00:49:00 +0300

    Initial Commit

---
+++ FactorioHelper/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FactorioHelper
+//
+//  Created by Sergey Vasilenko on 03.09.2020.
+//  Copyright © 2020 kudrykun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
